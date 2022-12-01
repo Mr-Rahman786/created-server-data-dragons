b1c72f96444090b8c1847c1cac2db9bef750f65d
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-12-01 23:01:16 +0800

    added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 app.use(cors())
 
 const categories = require('./Data/Categories.json')
-
+const news = require('./Data/news.json')
 app.get('/', (req, res) => {
     res.send('News APi Running')
 });
@@ -15,6 +15,27 @@ app.get('/news-categories', (req, res) => {
     res.send(categories);
 })
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news)
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectNews = news.find(n => n._id === id);
+    res.send(selectNews)
+})
+
 app.listen(port, () => {
-    console.log('Dragon news server running on port',port)
+    console.log('Dragon news server running on port', port)
 })
